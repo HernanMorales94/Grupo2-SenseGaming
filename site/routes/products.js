be7f0cca21f3d429316d142b4607cee6e0a167eb
Lisetangela <68971814+Lisetangela@users.noreply.github.com>
2020-10-03 23:50:06 -0300

    Update products.js
    moviendo a la carpeta de middleware

--- site/routes/products.js
+++ site/routes/products.js
@@ -2,13 +2,10 @@ const express = require("express")
 const router = express.Router()
 const productController = require("../controllers/productsController")
 
-let mw = (req,res.next) => {
-  console.log("pasando por el mw a nivel RUTA")
-  next();
-}
 
-router.get("/", mw, productController.listar)
-router.get("/detalle/:id", mw, productController.detalle)
+
+router.get("/", productController.listar)
+router.get("/detalle/:id", productController.detalle)
 router.get("/search",productController.search)
 
 module.exports=router
